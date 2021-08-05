2a34f54ca5b7b0e81b89e30064be1edf650674bb
raram5130 <starhigu@gmail.com>
2021-08-05 23:53:07 +0900

    count5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-ran = parseInt(Math.random()*100+1);
+ran = parseInt(Math.random() *100+1);
 count = 1;
 
 function guess() {
@@ -7,12 +7,18 @@ function guess() {
         alert('숫자를 입력하세요!');
     }
     if (num1 == ran) {
-    document.getElementById("textarea").innerHTML="정답!";
+        document.getElementById("textarea").innerHTML = "wow!정답!";
     } else {
         if(ran > num1){
-            document.getElementById("result").value=(num1 + "보다 UP!");
+            document.getElementById("result").value = (num1 + "보다 UPUP!");
     } else { 
-        document.getElementById("result").value=(num1 + "보다 DOWN!");
+        document.getElementById("result").value = (num1 + "보다 DOWN!");
+        }
     }
+    if(count == 5){
+        document.getElementById("btn").disabled = true;
+        document.getElementById("textarea").style.color = "tomato";
+        document.getElementById("textarea").innerHTML = "정답은 "+ran+" !";
     }
+    document.getElementById("trycount").value=((count++) + "번 도전했슴다.");
 }
